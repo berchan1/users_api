b42b8e6f31397a4ec79d7da2a09489afd7a054c1
Bernard Chaniewicz <bchaniewicz@netis.pl>
2023-03-26 16:46:19 +0200

    wip: users basic api
    add simple create user
    add password hasher

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -4,19 +4,53 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
 
+    private $doctrine;
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/api_user', name: 'api_user', methods: 'GET')]
-    public function api_user(): JsonResponse
+    public function api_user(EntityManagerInterface $entityManager): JsonResponse
     {
-//        dd('test');
+        $user = new User();
+        $user->setEmail('tt@wp.pl')->setFirstName('ten')->setLastName('shinhan')->setPassword('asdasdas');
+        $this->createUser($user);
+        $repository = $entityManager->getRepository(User::class);
+        $users = $repository->findAll();
+        dd($users);
         return $this->json(['test' => 'ok'], Response::HTTP_OK);
+    }
+
+    private function createUser(User $requestData): bool
+    {
+        $entityManager = $this->doctrine->getManager();
+
+        $user = (new User())
+            ->setEmail($requestData->getEmail())
+            ->setFirstName($requestData->getFirstName())
+            ->setlastName($requestData->getLastName());
+
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $requestData->getPassword())
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return true;
     }
 }
---
+++ back/src/DTO/User.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class User
+{
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    #[Assert\Unique]
+    private string $email;
+
+    private ?string $password = null;
+
+    #[Assert\NotBlank]
+    private string $firstName;
+
+    #[Assert\NotBlank]
+    private string $lastName;
+
+    /**
+     * Set email
+     *
+     * @param string $email Email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string|null $password Password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get first name
+     *
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set first name
+     *
+     * @param string $firstName First name
+     * @return $this
+     */
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get last name
+     *
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set last name
+     *
+     * @param string $lastName last name
+     * @return $this
+     */
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
